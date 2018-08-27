b844d9e27f5f9c6d80ec0d1359ec0c9ed30c9d43
Peter van der Noord <peterdunord@gmail.com>
2018-08-27 13:34:04 +0200

    Add vue (with SFC) setup in example

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,5 +1,12 @@
 import Graph from '../../src/lib/Graph';
+import App from './App.vue';
+
 
 const graph = new Graph();
 graph.addNode(2, 1);
-console.log(graph);
+
+var app = new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App />',
+});
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,16 +1,30 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const resolve = require('path').resolve;
+
 module.exports = {
   devServer: {
-    contentBase: './'
+  },
+  output: {
+    filename: './dist/bundle.js'
   },
   resolve: {
-    extensions: ['.js', '.ts'],
+    extensions: ['.js', '.ts', '.vue'],
   },
   module: {
     rules: [
       {
         test: /.ts$/,
         loader: 'ts-loader',
-      }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'],
+      },
     ]
-  }
+  },
+  plugins: [new VueLoaderPlugin()],
 };
